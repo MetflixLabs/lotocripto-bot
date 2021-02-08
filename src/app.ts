ff8ef31e33cda619ff39d2557d7286db0d9ec89e
eber404 <bodacamargo@gmail.com>
2021-02-08 18:55:50 -0300

    fix: socketio client test

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,23 @@ import { Server as socketio } from 'socket.io'
 import http from 'http'
 import cors from 'cors'
 import helmet from 'helmet'
+import { io as ioClient } from 'socket.io-client'
+import { sockets } from './sockets'
+import { SocketEnum } from './enums/SocketEnum'
 
 const app = express()
 const server = http.createServer(app)
 const io = new socketio({ path: '/socketio' }).listen(server)
 
+// client tests
+const socket = ioClient('http://localhost:4000', { path: '/socketio' })
+
+socket.emit(SocketEnum.JOIN_ROUND, { userId: 1, socketId: '398721hjas' })
+
+sockets(io)
+
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
 
-export { io, server }
+export { server }
