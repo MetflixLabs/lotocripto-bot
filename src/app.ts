57c55e702d81ca6a1680aa58dff4e8fbf1448502
eber404 <bodacamargo@gmail.com>
2021-02-14 12:38:29 -0300

    wip: bot login

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,22 @@ import helmet from 'helmet'
 import { io as ioClient } from 'socket.io-client'
 import { sockets } from './sockets'
 import { SocketEnum } from './enums/SocketEnum'
+import { AccessService } from './services/implementations/AccessService'
+const accessService = new AccessService()
 
 const app = express()
 const server = http.createServer(app)
 const path = process.env.SOCKETIO_PATH
 const io = new socketio({ path }).listen(server)
+
+const BOT_USER = process.env.BOT_USER
+const BOT_PASSWORD = process.env.BOT_PASSWORD
+
+// bot login
+if (!BOT_USER || !BOT_PASSWORD)
+  throw new Error('Login do bot não especificado nas variáveis de ambiente')
+
+accessService.login(BOT_USER, BOT_PASSWORD)
 
 // client tests
 const socket = ioClient('http://localhost:4000', { path: '/socketio' })
