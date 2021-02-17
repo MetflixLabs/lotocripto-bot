04793c5439367e4d2c56e2f7db0c03c5c9b9cbec
eber404 <bodacamargo@gmail.com>
2021-02-17 11:59:47 -0300

    fix: remove unnecessary socketio client lib

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ import { Server as socketio } from 'socket.io'
 import http from 'http'
 import cors from 'cors'
 import helmet from 'helmet'
-import { io as ioClient } from 'socket.io-client'
 import { sockets } from './sockets'
-import { SocketEnum } from './enums/SocketEnum'
 
 export const server = async (): Promise<http.Server> => {
   try {
@@ -21,11 +19,6 @@ export const server = async (): Promise<http.Server> => {
       },
       allowEIO3: true
     }).listen(server)
-
-    // client tests
-    // const socket = ioClient('http://localhost:4000', { path: '/ws' })
-
-    // socket.emit(SocketEnum.JOIN_ROUND, { userId: 1, socketId: '398721hjas' })
 
     sockets(io)
 
