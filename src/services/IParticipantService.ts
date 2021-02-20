e4794bde76416e143a508671cd52ef6d63caec04
eber404 <bodacamargo@gmail.com>
2021-02-20 00:37:40 -0300

    fix: participant service interface

--- src/services/IParticipantService.ts
+++ src/services/IParticipantService.ts
@@ -1,8 +1,10 @@
 import { IOutputResult } from '../interfaces/IOutputResult'
 import { IParticipant } from '../interfaces/IParticipant'
+import { IUser } from '../interfaces/IUser'
 
 export interface IParticipantService {
-  add(userId: string, socketId: string): Promise<IOutputResult>
-  update(id: string, participant: IParticipant): Promise<IOutputResult>
-  delete(userId: unknown, socketId?: string): Promise<IOutputResult>
+  add(userId: string, socketId: string): Promise<IOutputResult<IParticipant>>
+  update(id: string, participant: IParticipant): Promise<IOutputResult<IParticipant>>
+  delete(userId: unknown, socketId?: string): Promise<IOutputResult<IParticipant>>
+  getParticipantByTime(uptime: number): Promise<IOutputResult<IUser> | null>
 }
