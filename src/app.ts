9ff39e09cfa7c50055b1335f31b5f6f1969f8e89
pt-br <ltavares@yieldstreet.com>
2021-02-22 14:43:11 -0300

    feat: setup sentry

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,34 @@ import { Server as socketio } from 'socket.io'
 import http from 'http'
 import cors from 'cors'
 import helmet from 'helmet'
+import * as Sentry from '@sentry/node'
+import * as Tracing from '@sentry/tracing'
 import { sockets } from './sockets'
 
 export const server = async (): Promise<http.Server> => {
   try {
     const app = express()
+
+    Sentry.init({
+      dsn: process.env.SENTRY_KEY,
+      integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app })
+      ],
+
+      // We recommend adjusting this value in production, or using tracesSampler
+      // for finer control
+      tracesSampleRate: 0.5
+    })
+
+    // RequestHandler creates a separate execution context using domains, so that every
+    // transaction/span/breadcrumb is attached to its own Hub instance
+    app.use(Sentry.Handlers.requestHandler())
+    // TracingHandler creates a trace for every incoming request
+    app.use(Sentry.Handlers.tracingHandler())
+
     const server = http.createServer(app)
     const path = process.env.SOCKETIO_PATH
     const io = new socketio({
@@ -25,6 +48,9 @@ export const server = async (): Promise<http.Server> => {
     app.use(cors())
     app.use(helmet())
     app.use(express.json())
+
+    // The error handler must be before any other error middleware and after all controllers
+    app.use(Sentry.Handlers.errorHandler())
 
     return server
   } catch (error) {
