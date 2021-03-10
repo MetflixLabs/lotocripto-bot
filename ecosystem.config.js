1b9dfad29644f0b8eb6e95970fa46418d6e32bd3
pt-br <ltavares@yieldstreet.com>
2021-03-10 17:09:12 -0300

    fix: update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps : [{
     name: 'bot',
-    script: './src/server.ts',
+    script: 'node_modules/.bin/ts-node',
+    args: './src/server.ts',
     watch: '.',
     max_memory_restart: '450M'
   }]
